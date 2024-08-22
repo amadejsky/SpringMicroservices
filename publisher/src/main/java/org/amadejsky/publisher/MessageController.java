dada4651f19d85472c8b3e5e9eb84122896592bc
amadejsky <adrian.madejski@vp.pl>
2024-08-22 09:04:11 +0200

    Aplication publishing messages on rabbitAMQP, RabbitTemplate and configuration of connection with cloudAMQP

---
+++ publisher/src/main/java/org/amadejsky/publisher/MessageController.java
@@ -0,0 +1,22 @@
+package org.amadejsky.publisher;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+    private final RabbitTemplate rabbitTemplate;
+
+    public MessageController(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @GetMapping("/message")
+    public String sendMessage(@RequestParam String message) {
+
+        rabbitTemplate.convertAndSend("test", message);
+        return "Message thrown on RabbitAMQP";
+    }
+}
