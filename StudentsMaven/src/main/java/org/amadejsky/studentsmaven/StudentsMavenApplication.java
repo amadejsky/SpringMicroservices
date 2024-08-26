1ee4fd244a65ff53aef526764f739c7ecad1eab6
amadejsky <adrian.madejski@vp.pl>
2024-08-26 10:18:29 +0200

    Spring Eureka simple server configuration, connecting paralel instances with eureka

--- StudentsMaven/src/main/java/org/amadejsky/studentsmaven/StudentsMavenApplication.java
+++ StudentsMaven/src/main/java/org/amadejsky/studentsmaven/StudentsMavenApplication.java
@@ -5,11 +5,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import java.util.List;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class StudentsMavenApplication {
 //	@Value("${my.dogs}")
 //	private List<String> dogs;
